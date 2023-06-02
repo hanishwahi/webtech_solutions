9be09ac80cbf4d73682adde9bfb54ff2f59559b4
webtech <purposeresearch923@gmail.com>
2023-06-02 10:56:11 +0530

    webtech

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -13,7 +13,7 @@ function Banner() {
           <div className="home-hero-header">
             <h1 data-w-id="b3f9c3ce-27d6-c5d4-fcad-2662440c4edb"  className="home-hero-heading">
             YOUR COMPLETE BUSINESS SOLUTION<br/>
-              "WEBTECH SOLUTIONS" SERVICE
+            <span> WEBTECH SOLUTIONS SERVICE</span>
             </h1>
             <div data-w-id="fbc19c8e-292c-80bc-c002-20fb48dcf512"   className="home-hero-text">
             With our proficiency in web design, development and marketing, we excel at implementing the most effective web development process to help you create a prosperous online marketing plan. Our team will support you in promoting your company or business, connecting with your target audience, boosting website traffic, enhancing brand image, and driving more leads and sales.
--- src/Components/GrowBusiness.js
+++ src/Components/GrowBusiness.js
@@ -6,44 +6,48 @@ import Result from '../Images/results__2_.png'
 function DataMapping() {
   return (
     <>
-      <div className="tools-section wf-section">
+      <div className='container-fluid'>
+         
+          <div className="tools-section wf-section">
 
-        <div className="tools-header">
-          <h3 className="features-tools-heading">Sparkling Brands: Grow Your Business with Unforgettable Impact!</h3>
-          <div className="features-tools-header-text">Digital marketing is essential to define your brand values create a captivating website</div>
-        </div>
-        <div className="features-tools-grid">
-          <div className="features-tools-grid-contain row">
-            <div className="features-tools-grid-cell col-lg-4 col-md-6">
-              <div className='shadow py-5 px-3' >
-                <img
-                  src={Project_comp}
-                  loading="lazy" alt="" className="tools-grid-image" />
-                <h5 className="tools-grid-heading">100+ Projects Completed</h5>
-                <div className="tools-grid-text">till Now We Will Completed 100+ Projects</div>
-              </div>
+            <div className="tools-header">
+              <h3 className="features-tools-heading">Sparkling Brands: Grow Your Business with Unforgettable Impact!</h3>
+              <p className="features-tools-header-text">Digital marketing is essential to define your brand values create a captivating website</p>
             </div>
-            <div className="features-tools-grid-cell col-lg-4 col-md-6">
-              <div className='shadow py-5 px-3' >
-                <img
-                  src={Happy_client}
-                  loading="lazy" alt="" className="tools-grid-image" />
-                <h5 className="tools-grid-heading">90+ Happy Clients</h5>
-                <div className="tools-grid-text">Satisfied clients from various industries</div>
-              </div>
-            </div>
-            <div className="features-tools-grid-cell col-lg-4 col-md-6">
-              <div className='shadow py-5 px-3' >
-                <img
-                  src={Result}
-                  loading="lazy" alt="" className="tools-grid-image" />
-                <h5 className="tools-grid-heading">55+ Amazing Result</h5>
-                <div className="tools-grid-text">Delivering amazing results</div>
+            <div className="features-tools-grid">
+              <div className="features-tools-grid-contain row">
+                <div className="features-tools-grid-cell col-lg-4 col-md-6">
+                  <div className='shadow py-5 px-3' >
+                    <img
+                      src={Project_comp}
+                      loading="lazy" alt="" className="tools-grid-image" />
+                    <h5 className="tools-grid-heading">100+ Projects Completed</h5>
+                    <div className="tools-grid-text">till Now We Will Completed 100+ Projects</div>
+                  </div>
+                </div>
+                <div className="features-tools-grid-cell col-lg-4 col-md-6">
+                  <div className='shadow py-5 px-3' >
+                    <img
+                      src={Happy_client}
+                      loading="lazy" alt="" className="tools-grid-image" />
+                    <h5 className="tools-grid-heading">90+ Happy Clients</h5>
+                    <div className="tools-grid-text">Satisfied clients from various industries</div>
+                  </div>
+                </div>
+                <div className="features-tools-grid-cell col-lg-4 col-md-6">
+                  <div className='shadow py-5 px-3' >
+                    <img
+                      src={Result}
+                      loading="lazy" alt="" className="tools-grid-image" />
+                    <h5 className="tools-grid-heading">55+ Amazing Result</h5>
+                    <div className="tools-grid-text">Delivering amazing results</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      
 
     </>
   )
